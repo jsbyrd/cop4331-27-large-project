0b9664c90149157776f59af00114574e49f58a01
Joshua <jsbyrd02@gmail.com>
2023-10-15 20:42:39 -0400

    Modify server.js to produce static assets if in production

--- server.js
+++ server.js
@@ -7,6 +7,17 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production')
+{
+  // Set static folder
+  app.use(express.static('frontend/build'));
+  app.get('*', (req, res) =>
+  {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
+}
+
 app.set('port', (process.env.PORT || 5000));
 app.use((req, res, next) =>
 {
