c1f4154fcd6d0df58459b47e60be8db6d1470e08
Joshua <jsbyrd02@gmail.com>
2023-10-15 21:13:24 -0400

    Fix server.js to enable successful production builds

--- server.js
+++ server.js
@@ -12,26 +12,23 @@ if (process.env.NODE_ENV === 'production')
 {
   // Set static folder
   app.use(express.static('frontend/build'));
-  // app.get('*', (req, res) =>
-  // {
-  //   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  // });
 }
 
 app.set('port', (process.env.PORT || 5000));
-// app.use((req, res, next) =>
-// {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader(
-//   'Access-Control-Allow-Headers',
-//   'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-//   );
-//   res.setHeader(
-//   'Access-Control-Allow-Methods',
-//   'GET, POST, PATCH, DELETE, OPTIONS'
-//   );
-//   next();
-// });
+app.use((req, res, next) =>
+{
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+  'Access-Control-Allow-Headers',
+  'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+  );
+  res.setHeader(
+  'Access-Control-Allow-Methods',
+  'GET, POST, PATCH, DELETE, OPTIONS'
+  );
+  next();
+});
+
 // Use middleware
 app.use(cors());
 app.use(bodyParser.json());
