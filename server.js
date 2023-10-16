da33fc36a57a51be83f772bd6f005af6fa36cbee
Joshua <jsbyrd02@gmail.com>
2023-10-15 21:08:17 -0400

    Test2

--- server.js
+++ server.js
@@ -7,41 +7,38 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-console.log("Hello");
-
 // Server static assets if in production
 if (process.env.NODE_ENV === 'production')
 {
-  console.log("Aloha");
   // Set static folder
   app.use(express.static('frontend/build'));
-  app.get('*', (req, res) =>
-  {
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
-  });
+  // app.get('*', (req, res) =>
+  // {
+  //   res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  // });
 }
 
 app.set('port', (process.env.PORT || 5000));
-app.use((req, res, next) =>
-{
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader(
-  'Access-Control-Allow-Headers',
-  'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  res.setHeader(
-  'Access-Control-Allow-Methods',
-  'GET, POST, PATCH, DELETE, OPTIONS'
-  );
-  next();
-});
+// app.use((req, res, next) =>
+// {
+//   res.setHeader('Access-Control-Allow-Origin', '*');
+//   res.setHeader(
+//   'Access-Control-Allow-Headers',
+//   'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+//   );
+//   res.setHeader(
+//   'Access-Control-Allow-Methods',
+//   'GET, POST, PATCH, DELETE, OPTIONS'
+//   );
+//   next();
+// });
+// Use middleware
+app.use(cors());
+app.use(bodyParser.json());
 
 // Import api endpoints
 const usersRouter = require("./controllers/users");
 
-// Use middleware
-app.use(cors());
-app.use(bodyParser.json());
 // Use routes
 app.use("/api/users", usersRouter);
 
