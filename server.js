e3e1f2a3e41f2ca648f08d92754eda311dd5dfe0
Joshua <jsbyrd02@gmail.com>
2023-10-21 17:27:35 -0400

    Add potential fix for heroku

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ if (process.env.NODE_ENV === 'production')
 {
   // Set static folder
   app.use(express.static('frontend/build'));
+  app.get('*', (req, res) =>
+  {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+  });
 }
 
 app.set('port', (process.env.PORT || 5000));
@@ -35,7 +39,7 @@ app.use(bodyParser.json());
 
 // Import api endpoints
 const usersRouter = require("./api/users");
-const quizzesRouter = require("./api/quizzes");
+// const quizzesRouter = require("./api/quizzes");
 
 // Use routes
 app.use("/api/users", usersRouter);
