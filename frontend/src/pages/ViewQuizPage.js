80f4ae5427fbc350a35fa28851abd63e1b16ea52
Joshua <jsbyrd02@gmail.com>
2023-11-05 17:52:20 -0500

    Add quiz title, quiz options, flashcards, and flashcard navigation on ViewQuizPage

--- frontend/src/pages/ViewQuizPage.js
+++ frontend/src/pages/ViewQuizPage.js
@@ -3,16 +3,45 @@ import axios from 'axios';
 import { useParams } from 'react-router-dom';
 import MenuHeader from '../components/MenuHeader';
 import DefaultFooter from '../components/DefaultFooter';
+import './ViewQuizPage.css';
 
 const ViewQuizPage = () => {
 
   const { quizID } = useParams();
-  const dummyQuestions = [{Question: "How are you today?", _id: "1"},
-                          {Question: "This is question two", _id: "2"}, 
-                          {Question: "Are you stupid?", _id: "3"},
-                          {Question: "This is very very dumb", _id: "4"}];
-  const [questions, setQuestions] = useState(dummyQuestions);
+  // const dummyQuestions = [{Question: "How are you today?", _id: "1"},
+  //                         {Question: "This is question two", _id: "2"}, 
+  //                         {Question: "Are you stupid?", _id: "3"},
+  //                         {Question: "This is very very dumb", _id: "4"}];
+  const [quizInfo, setQuizInfo] = useState({});
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestion, setCurrentQuestion] = useState(-1);
   const [isLoading, setIsLoading] = useState(true);
+
+  const moveFlashCardForward = (e) => {
+    e.preventDefault();
+    if (currentQuestion >= questions.length - 1) {
+      return;
+    }
+    setCurrentQuestion(currentQuestion + 1);
+  }
+
+  const moveFlashCardBackward = (e) => {
+    e.preventDefault();
+    if (currentQuestion <= 0) {
+      return;
+    }
+    setCurrentQuestion(currentQuestion - 1);
+  }
+
+  const fetchQuizInfo = async () => {
+    const fetchParams = {
+      id: quizID
+    }
+    const res = await axios.post(`https://cop4331-27-c6dfafc737d8.herokuapp.com/api/quizzes/get/`, fetchParams);
+    if (res !== undefined && res.data.result.length !== 0) {
+      setQuizInfo(res.data.result[0]);
+    }
+  }
 
   const fetchQuestions = async () => {
     const fetchParams = {
@@ -20,41 +49,50 @@ const ViewQuizPage = () => {
       quizId: quizID
     }
     const res = await axios.post(`https://cop4331-27-c6dfafc737d8.herokuapp.com/api/questions/search/`, fetchParams);
-    console.log(res);
-    if (res !== undefined && res.data.result.length !== 0)
+    if (res !== undefined && res.data.result.length !== 0) {
       setQuestions(res.data.result);
+      setCurrentQuestion(0);
+    }
   }
 
 
-  // Fetch All Questions
+  // Fetch Quiz info and all Questions related to that quiz
   useEffect(() => {
+    fetchQuizInfo();
     fetchQuestions();
     setIsLoading(false);
-  }, [])
-
-  console.log(questions);
+    
+  }, []);
 
   return (
-    <div>
+    <div id='vqp-container'>
       <MenuHeader />
-      <p>Quiz ID: {`${quizID}`}</p>
-      <p>isLoading? {`${isLoading}`}</p>
-      <p>Questions: </p>
       <div id='vqp-body-container'>
         <div id='vqp-body'>
-          <p id='vqp-quiz-title'>Quiz Title</p>
+          <p id='vqp-quiz-title'>{quizInfo.Name}</p>
           <div id='vqp-quiz-options'>
-            <button>Test</button>
-            <button>Save</button>
-            <button>Add Question</button>
+            <button className='vqp-qo'>Take Test</button>
+            <button className='vqp-qo'>Save Quiz</button>
+            <button className='vqp-qo'>Add Question</button>
           </div>
-          <div>
-            BIG QUESTION
+          <div id='vqp-flashcard'>
+            <div id='flip-card-inner'>
+              <div className='flip-card-side' id='flip-card-front'>
+                <p id='vqp-flashcard-q'>
+                  {(questions.length === 0) ? "No Questions :(" : `${questions[currentQuestion].Question}`}
+                </p>
+              </div>
+              <div className='flip-card-side' id='flip-card-back'>
+              <p id='vqp-flashcard-q'>
+              {(questions.length === 0) ? "No Answers :(" : "No Answers :("}
+                </p>
+              </div>
+            </div>
           </div>
-          <div>
-            <button>{"<-"}</button>
-            <p>5/64</p>
-            <button>{"->"}</button>
+          <div id='vqp-flashcard-nav'>
+            <button className='nav-btn' id='nav-btn-back' onClick={moveFlashCardBackward}>{"←"}</button>
+            <p id='flashcard-nav-count'>{`${currentQuestion + 1} / ${isLoading ? 0 : questions.length}`}</p>
+            <button className='nav-btn' id='nav-btn-forward' onClick={moveFlashCardForward}>{"→"}</button>
           </div>
           <ul>
             {questions.map((q) => {
