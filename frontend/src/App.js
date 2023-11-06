bf75b949004470672f079170003372e31c4836be
Parker Waller <pwaller815@icloud.com>
2023-11-05 22:22:13 -0500

    Added functionality to search bar.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -14,11 +14,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/search" element={<SearchedPage />} />
         <Route path="/" element={<DefaultPage />} />
         <Route path="login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/menu" element={<MenuPage />} />
+        <Route path="/search/:query" element={<SearchedPage />} />
         <Route path="/viewquiz/:quizID" element={<ViewQuizPage />} />
       </Routes>
     </BrowserRouter>
