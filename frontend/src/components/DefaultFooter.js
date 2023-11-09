d71c8036f5e809963a3c0970f2af369871663710
Joshua <jsbyrd02@gmail.com>
2023-11-09 15:09:05 -0500

    Change header and footer from position sticky to position static

--- frontend/src/components/DefaultFooter.js
+++ frontend/src/components/DefaultFooter.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const DefaultFooter = () => {
   return (
-    <footer class="bg-light text-center text-lg-start position-sticky bottom-0">
+    <footer class="bg-light text-center text-lg-start bottom-0">
       <div class="text-center p-3 text-light" style={{background: "rgba(30,26,80,255)"}}>
         © 2023 Copyright: QuizWiz
       </div>
