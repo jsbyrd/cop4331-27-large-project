2cf76eb2efea4ea7aad0612da26d4ab3fd1bb7fc
dshelton2001 <de690776@ucf.edu>
2023-11-13 17:24:44 -0500

    Added packages that allow for cookies

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-var cookieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
