45a710440b5ab68a783747d5f4a3ff26f77c9574
Joshua Currie <johndoe@example.com>
2023-11-22 14:54:58 -0500

    test page shows answers*

--- frontend/src/pages/TestPage.js
+++ frontend/src/pages/TestPage.js
@@ -11,28 +11,10 @@ const TestPage = () => {
 
     const { quizID } = useParams();
 
-    const [quizInfo, setQuizInfo] = useState({Name: "Failed to load quiz..."});
     const [questions, setQuestions] = useState([]);
-    // const [answers, setAnswers] = useState([]);
-    let answers = new Array();
-
-    const [currentQuestion, setCurrentQuestion] = useState(-1);
-    const [isLoading, setIsLoading] = useState(true);
-
-    const fetchQuizInfo = async () => {
-        const fetchParams = {
-        id: quizID
-        }
-        try {
-        const res = await axios.post(path.buildPath('/api/quizzes/get'), fetchParams);
-        if (res !== undefined && res.data.result.length !== 0) {
-            setQuizInfo(res.data.result[0]);
-        }
-        }
-        catch {
-        return;
-        }
-    }
+    const [answers, setAnswers] = useState([[]]);
+    const [rightAnswers, setRightAnswers] = useState([]);
+    const [wrongAnswers, setWrongAnswers] = useState([]);
 
     const fetchQuestions = async () => {
         const fetchParams = {
@@ -40,70 +22,99 @@ const TestPage = () => {
         quizId: quizID
         }
         try {
-        const res = await axios.post(path.buildPath('/api/questions/search'), fetchParams);
+        const res = await axios.post(`https://cop4331-27-c6dfafc737d8.herokuapp.com/api/questions/search/`, fetchParams);
         if (res !== undefined && res.data.result.length !== 0) {
             setQuestions(res.data.result);
-            setCurrentQuestion(0);
         }
         } catch {
+            console.log("fetch question ERROR :(");
         return;
         }
     }
 
     const fetchAnswers = async () => {
-        let numQuestions = questions.length;
-
-        for (let i = 0; i < numQuestions; i++)
-        {
-            // console.log(i); // debug
-            // console.log(questions[i]._id); // debug
-            // console.log(quizID); // debug
-
+        for (let i = 0; i < questions.length; i++) {
             const fetchParams = {
-                QuestionId: questions[i]._id
-                }
-                try {
+                questionId: questions[i]._id
+            };
+            try {
                 const res = await axios.post(`https://cop4331-27-c6dfafc737d8.herokuapp.com/api/answers/get/`, fetchParams);
                 if (res !== undefined && res.data.result.length !== 0) {
-                    // console.log(res.data.result);
-                    // setAnswers(res.data.result);      
-                    
-                    // very uneffiecent
-                    for (let j = 0; j < res.data.result.length; j++)
-                    {
-                        if (!res.data.result[j].WrongAnswer)
-                        {
-                            answers[i] = res.data.result[j];
-                            console.log(answers[i]);
-                        }
-                    }
+                    setAnswers(prevAnswers => {
+                        const newAnswers = [...prevAnswers];
+                        newAnswers[i] = res.data.result;
+                        return newAnswers;
+                    });
                 }
-                } catch {
-                    console.log("ERROR :(");
+            } catch {
+                console.log("fetch answers ERROR :(");
                 return;
+            }
+        }
+    };
+
+    const storeCorrectAnswers = async () => {
+
+        for (let i = 0; i < answers.length; i++)
+        {
+            for (let j = 0; j < answers[i].length; j++)
+            {
+                if (!answers[i][j].WrongAnswer)
+                {
+                    rightAnswers[i] = answers[i][j].Answer;
                 }
+            }
+        }
+    }
+
+    const storeWrongAnswers = async () => {
+
+        let temp = 0;
+
+        for (let i = 0; i < answers.length; i++)
+        {
+            for (let j = 0; j < answers[i].length; j++)
+            {
+                if (answers[i][j].WrongAnswer)
+                {
+
+                    if (wrongAnswers.length < i + 1)
+                    {
+                        setWrongAnswers(prevArray => [...prevArray, []]);
+                    }
+
+                    setWrongAnswers(prevArray => {
+                        const newArray = [...prevArray];
+                
+                        newArray[i] = [...newArray[i]];
+                
+                        newArray[i][j + 1] = answers[i][j].Answer; // fix newArray coords
+                
+                        return newArray;
+                    });
+                temp++;
+                }
+            }
         }
     }
 
     useEffect(() => {
-        const fetchData = async () => {
-            await fetchQuizInfo();
-            await fetchQuestions();
-            setIsLoading(false);
-        };
-    
-        fetchData();
+        fetchQuestions();
     }, []);
-    
-
+   
     useEffect(() => {
         if (questions.length > 0) {
             fetchAnswers();
         }
-        
     }, [questions]);
 
-    return ( 
+    useEffect(() => {
+        storeCorrectAnswers();
+        storeWrongAnswers();
+    }, [answers]);
+
+
+    return (
         <div>
             <MenuHeader />
             <div id="test-page-body">
@@ -113,13 +124,13 @@ const TestPage = () => {
                     <div key={index} className="question-container">
                         <p className='question-number'>Question {index + 1}</p>
                         <p className='question-text'>{questionItem.Question}</p>
-                        <p className='question-text'>(temporary) question Id: {questionItem._id}</p> 
                         <div className='answer-choices'>
                             <button className='answer-choice'>
                                 <p>
-                                    
+                                    {rightAnswers[index]}
                                 </p>
                             </button>
+
 
                             <button className='answer-choice'>
                                 <p>
@@ -127,17 +138,20 @@ const TestPage = () => {
                                 </p>
                             </button>
 
+
                             <button className='answer-choice'>
                                 <p>
                                     Answer placeholder :D
                                 </p>
                             </button>
 
+
                             <button className='answer-choice'>
                                 <p>
-                                    Answer placeholder :D
+                                    {wrongAnswers[index]}
+                                    {console.log(wrongAnswers[index])}
                                 </p>
-                            </button>                     
+                            </button>                    
                         </div>
                     </div>
                     ))
@@ -145,6 +159,11 @@ const TestPage = () => {
                     // Display a message if there are no questions
                     <p>No questions available.</p>
                 )}
+                <div className="test-result">
+                        <button className="submit-button">
+                            Submit
+                        </button>
+                </div>
             </div>
             <DefaultFooter />
         </div>
