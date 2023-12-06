d13e39a27d34a28ffdd246506154574330ab360a
Parker Waller <pwaller815@icloud.com>
2023-12-06 02:24:02 -0500

    Reset password functionality complete.

--- frontend/src/pages/ForgotPasswordPage.js
+++ frontend/src/pages/ForgotPasswordPage.js
@@ -1,34 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+const path = require('../components/Path.js');
 
 const ForgotPasswordPage = () => {
 
-    var RECIPIENT_EMAIL;
+  var RECIPIENT_EMAIL;
+  const [RECIPIENT_USERNAME, setRECIPIENT_USERNAME] = useState('');
+  const [recievedUser, setRecievedUser] = useState(false);
+  const [emailSent, setEmailSent] = useState(false);
 
-    const ForgotPassword = async () => {
-        const resetLink = 'http://localhost:3000/doreset';
-        console.log(RECIPIENT_EMAIL.value);
-        try {
-            const response = await axios.post('http://localhost:5000/reset-pass', {
-              recipientEmail: RECIPIENT_EMAIL.value,
-              resetLink: resetLink
-            });
-            console.log('Email sent successfully:', response.data);
-          } catch (error) {
-            console.error('Error sending email:', error.response.data);
-          }
+  const UpdateUsername = async () => {
+    setRecievedUser(true);
+  }
+
+  const ForgotPassword = async () => {
+    const resetLink = "https://cop4331-27-c6dfafc737d8.herokuapp.com/doreset";
+    const queryParams = {
+      login: RECIPIENT_USERNAME
+    };
+    const encodedParams = [];
+    // Encode each query parameter
+    Object.entries(queryParams).forEach(([key, value]) => {
+      encodedParams.push(`${key}=${encodeURIComponent(value)}`);
+    });
+    // Construct the URI with encoded query parameters
+    const uriWithParams = `${resetLink}?${encodedParams.join("&")}`;
+    try {
+      const response = await axios.post(path.buildPath('/reset-pass'), {
+        recipientEmail: RECIPIENT_EMAIL.value,
+        resetLink: uriWithParams
+      });
+      console.log('Email sent successfully:', response.data);
+      setEmailSent(true);
+    } catch (error) {
+      console.error('Error sending email:', error.response.data);
     }
+  }
 
-    return (
-        <div className="d-flex flex-column align-items-center justify-content-center" style={{ height: "100vh", backgroundColor: "#340e57", color: "white" }}>
-            <p style={{ textAlign: "center" }}>Enter the email address associated with the account.</p>
-            <input type="text" id="loginName" class="login-user-input-field" placeholder="wizard@email.com" style={{height: '50px', width: '400px'}} ref={(c) => RECIPIENT_EMAIL = c} /><br />
-            <button style={{ marginTop: "0px", backgroundColor: "#693c72", color: "white", padding: "10px 20px", border: "none", cursor: "pointer", fontSize: "12px" }}
+  return (
+    <div className="d-flex flex-column align-items-center justify-content-center" style={{ height: "100vh", backgroundColor: "#340e57", color: "white" }}>
+      {recievedUser && (
+        <><p style={{ textAlign: "center" }}>Enter the email address associated with the account.</p>
+          <input type="text" id="loginName" class="login-user-input-field" placeholder="wizard@email.com" style={{ height: '50px', width: '400px' }} ref={(c) => RECIPIENT_EMAIL = c} /><br />
+          <div style={{ height: "20px" }}>
+            {emailSent && (
+              <p style={{ marginTop: "-20px", color: "white" }}>An email has been sent. Please check your inbox to reset your password.</p>
+            )}
+          </div>
+          <div style={{ height: "20px" }}>
+            {!emailSent && (
+              <button style={{ marginTop: "0px", backgroundColor: "#693c72", color: "white", padding: "10px 20px", border: "none", cursor: "pointer", fontSize: "12px" }}
                 onClick={event => ForgotPassword()}>
                 Submit
+              </button>
+            )}
+          </div></>
+      )}
+      {recievedUser == false && (
+        <><p style={{ textAlign: "center" }}>Enter the username associated with the account.</p>
+          <input type="text" id="loginName" class="login-user-input-field" placeholder="Wizard54" style={{ height: '50px', width: '400px' }} onChange={(e) => setRECIPIENT_USERNAME(e.target.value)} /><br />
+          <div style={{ height: "20px" }}></div>
+          <div style={{ height: "20px" }}>
+            <button style={{ marginTop: "0px", backgroundColor: "#693c72", color: "white", padding: "10px 20px", border: "none", cursor: "pointer", fontSize: "12px" }}
+              onClick={event => UpdateUsername()}>
+              Submit
             </button>
-        </div>
-    );
+          </div>
+        </>
+      )}
+    </div>
+  );
 };
 
 export default ForgotPasswordPage;
--- frontend/src/pages/ResetPasswordPage.js
+++ frontend/src/pages/ResetPasswordPage.js
@@ -1,12 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
+const path = require('../components/Path.js');
 
 const ResetPasswordPage = () => {
 
-    return (
-        <div className="d-flex flex-column align-items-center justify-content-center" style={{ height: "100vh", backgroundColor: "#340e57", color: "white" }}>
-          <p>Reset Password</p>
-        </div>
-    );
+  const location = useLocation();
+  const queryParams = new URLSearchParams(location.search);
+  const login = queryParams.get("login");
+  const [message, setMessage] = useState('')
+  const [NEWPASS1, setNEWPASS1] = useState('');
+  const [NEWPASS2, setNEWPASS2] = useState('');
+
+  const doRecovery = async event => {
+    console.log(NEWPASS1);
+    console.log(NEWPASS2);
+    console.group(login);
+    if (NEWPASS1 !== NEWPASS2) {
+      setMessage("Entered passwords do not match.");
+      return;
+    }
+    else {
+      var obj = { login: login, newPassword: NEWPASS1 };
+      var js = JSON.stringify(obj);
+      try {
+        const response = await fetch(path.buildPath('/api/users/recovery'), { method: 'post', body: js, headers: { 'Content-Type': 'application/json' } });
+        var res = JSON.parse(await response.text());
+        console.log(res);
+        if (res.error !== "") {
+          setMessage("Unable to update password.");
+        }
+        else {
+          window.location.href = '/login';
+          return 0;
+        }
+      }
+      catch (e) {
+        alert(e.toString());
+        return;
+      }
+    }
+  }
+
+  return (
+    <div className="d-flex flex-column align-items-center justify-content-center" style={{ height: "100vh", backgroundColor: "#340e57", color: "white" }}>
+      <p style={{ textAlign: "center" }}>Enter the new password.</p>
+      <input type="text" id="loginName" class="login-user-input-field" placeholder="..." style={{ height: '50px', width: '400px' }} onChange={(e) => setNEWPASS1(e.target.value)} /><br />
+      <p style={{ textAlign: "center" }}>Renter the new password.</p>
+      <input type="text" id="loginName" class="login-user-input-field" placeholder="..." style={{ height: '50px', width: '400px' }} onChange={(e) => setNEWPASS2(e.target.value)} /><br />
+      <button style={{ marginTop: "0px", backgroundColor: "#693c72", color: "white", padding: "10px 20px", border: "none", cursor: "pointer", fontSize: "12px" }}
+        onClick={event => doRecovery()}>
+        Submit
+      </button>
+      <div style={{ height: "20px" }}>
+        <br></br>
+        <p style={{ color: "red" }}>{message}</p>
+      </div>
+    </div>
+  );
 };
 
 export default ResetPasswordPage;
